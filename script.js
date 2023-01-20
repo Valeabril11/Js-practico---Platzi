3297bf92802648dd7e3595e9bfad7a1fa50f7456
Valeria Molina <102936189+Valeabril11@users.noreply.github.com>
2023-01-20 15:55:36 -0300

    primeros commits

---
+++ script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const result = document.querySelector('#result')
+
+form.addEventListener('click', btnOnclick)
+
+function btnOnclick(event) {
+    console.log('evento de la funcion')
+    event.preventDefault();
+    const suma = Number(input1.value) + Number(input2.value);
+   result.innerText ='resultado: ' + suma
+
+}
